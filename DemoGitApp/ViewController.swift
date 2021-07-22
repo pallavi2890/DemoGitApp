f1a7771872d4219813eba05dac62a99aabf9ec35
Pallavi Agarwal <pallavi.agarwal@mediaagility.com>
2021-07-22 16:29:22 +0800

    Initial Commit

---
+++ DemoGitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGitApp
+//
+//  Created by Pallavi Agarwal on 22/07/21.
+//  Copyright © 2021 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
